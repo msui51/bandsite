4c1352ead9951d917426f22b186075c66a7f9800
Maria Sui <maria.sui51@gmail.com>
2023-05-04 20:28:09 -0400

    added comments to shows js

--- maria-sui-bandsite/scripts/build-shows-page.js
+++ maria-sui-bandsite/scripts/build-shows-page.js
@@ -14,6 +14,8 @@
     ul.classList.add("main__list");
     main.appendChild(ul);
 
+//function retrieve shows data    
+
 function showsData(){
     axios
     .get("https://project-1-api.herokuapp.com/showdates?api_key=c8a23e5a-96f4-4b41-bc3e-a5f97cb8f66a")
@@ -23,60 +25,64 @@ function showsData(){
 }
 showsData()
 
-    function shows(showsArray){
-        for(let i=0; i<showsArray.length; i++){
+/*
+function to loop through each show
+and assign elements, classes, append, etc.
+*/
+function shows(showsArray){
+    for(let i=0; i<showsArray.length; i++){
             
-            const li= document.createElement("li");
-            li.classList.add("main__item");
+    const li= document.createElement("li");
+    li.classList.add("main__item");
 
-            let date = document.createElement("h3");
-            date.classList.add("main__item-title");
-            date.classList.add("main__item-title--disappear");
+    let date = document.createElement("h3");
+    date.classList.add("main__item-title");
+    date.classList.add("main__item-title--disappear");
 
-            let place= document.createElement("h3");
-            place.classList.add("main__item-title");
-            place.classList.add("main__item-title--disappear");
+    let place= document.createElement("h3");
+    place.classList.add("main__item-title");
+    place.classList.add("main__item-title--disappear");
 
-            let location= document.createElement("h3");
-            location.classList.add("main__item-title");
-            location.classList.add("main__item-title--disappear");
+    let location= document.createElement("h3");
+    location.classList.add("main__item-title");
+    location.classList.add("main__item-title--disappear");
 
-            let dateContent= document.createElement("p");
-            dateContent.classList.add("main__item-detail");
-            dateContent.classList.add("main__item-detail--bold");
+    let dateContent= document.createElement("p");
+    dateContent.classList.add("main__item-detail");
+    dateContent.classList.add("main__item-detail--bold");
 
-            let placeContent= document.createElement("p");
-            placeContent.classList.add("main__item-detail");
+    let placeContent= document.createElement("p");
+    placeContent.classList.add("main__item-detail");
 
-            let locationContent= document.createElement("p");
-            locationContent.classList.add("main__item-detail");
+    let locationContent= document.createElement("p");
+    locationContent.classList.add("main__item-detail");
 
             
-            date.innerText= "Date";
-            place.innerText= "Venue";
-            location.innerText= "Location";
-            showDate=new Date(showsArray[i].date);
-            dateContent.innerText= showDate.toDateString();
-            placeContent.innerText=showsArray[i].place;
-            locationContent.innerText=showsArray[i].location;
+    date.innerText= "Date";
+    place.innerText= "Venue";
+    location.innerText= "Location";
+    showDate=new Date(showsArray[i].date);
+    dateContent.innerText= showDate.toDateString();
+    placeContent.innerText=showsArray[i].place;
+    locationContent.innerText=showsArray[i].location;
 
-            li.appendChild(date);
-            li.appendChild(dateContent);
+    li.appendChild(date);
+    li.appendChild(dateContent);
             
-            li.appendChild(place);
-            li.appendChild(placeContent);
+    li.appendChild(place);
+    li.appendChild(placeContent);
 
-            li.appendChild(location);
-            li.appendChild(locationContent);
+    li.appendChild(location);
+    li.appendChild(locationContent);
 
-            const button= document.createElement("button");
-            button.classList.add("main__cta-button");
-            button.innerText="Buy Tickets";
-            li.appendChild(button);
+    const button= document.createElement("button");
+    button.classList.add("main__cta-button");
+    button.innerText="Buy Tickets";
+    li.appendChild(button);
         
-            ul.appendChild(li);
-        };
-    }
+    ul.appendChild(li);
+    };
+}
 
     // adding hover and select state for shows
 
